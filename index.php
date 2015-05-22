66c90144b35a371102cd4ce7c792dac4bc6e0ded
chedie <chedie@gmail.com>
2015-05-22 10:44:31 -0600

    Add draft index.php

---
+++ index.php
@@ -0,0 +1,70 @@
+<html lang="en">
+<head>
+<title>Web Humanization &amp; Optimization</title>
+
+<META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">
+<meta name="viewport" content="width=device-width, initial-scale=1"/>
+<meta name="title" content="Web Humanization & Optimization">
+<meta name="description" content=""/>
+<meta name="author" content="Viiworks Inc."/>
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+
+
+
+<!-- Include roboto.css to use the Roboto web font, material.css to include the theme and ripples.css to style the ripple effect -->
+<link href="dist/css/roboto.min.css" rel="stylesheet">
+<link href="dist/css/material.min.css" rel="stylesheet">
+<link href="dist/css/ripples.min.css" rel="stylesheet">
+
+
+<link rel="apple-touch-icon" sizes="57x57" href="images/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="images/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="images/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="images/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="images/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="images/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="images/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="images/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="images/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="images/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="images/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png">
+<link rel="manifest" href="/manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="images/ms-icon-144x144.png">
+</head>
+
+<body>
+
+<h1>You can add your site here.</h1>
+
+<h2>To ensure that material-design theme is working, check out the buttons below.</h2>
+
+<h3 class="text-muted">If you can see the ripple effect on clicking them, then you are good to go!</h3>
+
+<p class="bs-component">
+	<a href="javascript:void(0)" class="btn btn-default">Default</a>
+    <a href="javascript:void(0)" class="btn btn-primary">Primary</a>
+    <a href="javascript:void(0)" class="btn btn-success">Success</a>
+    <a href="javascript:void(0)" class="btn btn-info">Info</a>
+    <a href="javascript:void(0)" class="btn btn-warning">Warning</a>
+    <a href="javascript:void(0)" class="btn btn-danger">Danger</a>
+    <a href="javascript:void(0)" class="btn btn-link">Link</a>
+</p>
+
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.10.2.min.js"></script>
+<script src="dist/js/ripples.min.js"></script>
+<script src="dist/js/material.min.js"></script>
+<script>
+	$(document).ready(function() {
+    // This command is used to initialize some elements and make them work properly
+    	$.material.init();
+    });
+</script>
+</body>
+</html>
